ac525edbf6cf49dfb320ca26b89bdcd62f283ab9
taobao1875 <taobao1875@163.com>
2020-06-10 16:45:22 +0800

    123

--- backend/web/index.php
+++ backend/web/index.php
@@ -15,5 +15,4 @@ $config = yii\helpers\ArrayHelper::merge(
     require __DIR__ . '/../config/main.php',
     require __DIR__ . '/../config/main-local.php'
 );
-var_dump(123);die;
 (new yii\web\Application($config))->run();
