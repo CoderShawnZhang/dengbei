9bfaf0268ee6f0ae7f37a6393b21b21acda1ef1f
taobao1875 <taobao1875@163.com>
2020-07-16 14:45:27 +0800

    rbac init cmd

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -1,12 +1,27 @@
 <?php
 return [
     'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=dengbei;port=3306',
+            'username' => 'root',
+            'password' => '123456',
+            'charset' => 'utf8',
+        ],
+        //旧的搜灯数据库
+        'old_sodeng' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=sodeng;port=3306',
+            'username' => 'root',
+            'password' => '123456',
+            'charset' => 'utf8',
+        ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.111
+            // for the mailer to send real emails.
             'useFileTransport' => true,
         ],
     ],
